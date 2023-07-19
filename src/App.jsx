954435f1c3c6c45ad34667fe2139266a68312cad
mat_dweb <mat.dweb28@gmail.com>
2023-07-19 12:44:27 -0600

    Create MusicCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './input.css'
+import MusicCard from './MusicCard'
 
 function App() {
 
+  const array = [1,2,3,4,5,6];
+
   return (
     <>
-      <div className=''>
-        <h1 className=''>Hello World of React</h1>
-        <button className='btn'>DaisyUI button</button>
+      <div className='bg-gray-950 w-full h-full'>
+
+        <div className='p-5'>
+          <input type="text" name='singer' placeholder="Type some singer here" className="input input-bordered input-primary w-full max-w-xs" />
+          <button className='btn'>Search</button>
+        </div>
+
+        <div className='flex flex-row flex-wrap gap-4 p-5'>
+          {array.map((element) => {
+            return <MusicCard/>
+          })}
+        </div>
       </div>
     </>
   )
