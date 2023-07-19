1dd7582dae91116cb657adddd2e911835ad8f325
mat_dweb <mat.dweb28@gmail.com>
2023-07-19 09:37:43 -0600

    set up tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import './input.css'
 
 function App() {
 
   return (
     <>
-      
+      <div className=''>
+        <h1 className='outline outline-gray-900 outline-4 underline'>Hello World of React</h1>
+      </div>
     </>
   )
 }
