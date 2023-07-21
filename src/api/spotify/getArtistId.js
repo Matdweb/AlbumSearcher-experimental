51684856517676bc590dfac36e2f902061e0165e
mat_dweb <mat.dweb28@gmail.com>
2023-07-21 08:20:49 -0600

    Export fetch function that retieves an artist ID

---
+++ src/api/spotify/getArtistId.js
@@ -0,0 +1,11 @@
+//this function getArtistId retrurns the ID of the fisrt artist that is returned in base of a search Input of the user
+import searchParameters from './GETSearchParamsWithAccessToken';
+
+const getArtistId = async (searchInput) => {
+    let artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+        .then(response => response.json())
+        .then(data => { return data.artists.items[0].id });
+    return await artistID;
+}
+
+export default getArtistId;
