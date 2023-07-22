c3b87bb4bf373028bab0693fcd3e37f103a18874
mat_dweb <mat.dweb28@gmail.com>
2023-07-22 02:33:25 -0600

    Get Access token and retrun a searchParams obj with the token

--- src/api/spotify/GETSearchParamsWithAccessToken.js
+++ src/api/spotify/GETSearchParamsWithAccessToken.js
@@ -1,11 +1,16 @@
 //This parameters are used to make a get request to the Spotify API with a access token
+import getAccessToken from "./getAccessToken";
 
-const accessToken = null;
-
-export default {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + accessToken
+const getSearchParams = async () => {
+    const accessToken = await getAccessToken();
+    console.log(accessToken);
+    return {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        }
     }
-}
+};
+
+export default getSearchParams;
